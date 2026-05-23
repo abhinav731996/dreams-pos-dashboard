43b9f13dc93607bf0a480c69a6e92f65a929f3f6
Abhinav <abhinav731996@gmail.com>
2026-05-23 14:04:11 +0530

    creating outlook

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap-icons/font/bootstrap-icons.css";
+// import "./assets/scss/min.scss";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
 
-import "./assets/scss/main.scss";
 
-import App from './App.jsx'
+ReactDOM.createRoot(document.getElementById("root")).render(
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <Provider store={store}>
+  <App />
+</Provider>
+
+);
